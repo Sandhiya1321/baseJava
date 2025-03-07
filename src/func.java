6b3e1377937697c00aa24afcafca4f3dbdecb1dc
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-03-07 09:44:33 +0530

    base

---
+++ src/func.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class func {
+    public static void main(String[] args) {
+        sum();
+    }
+    public static void sum(){
+        Scanner scan=new Scanner(System.in);
+        int a,b,c;
+        System.out.print("enter a:");
+        a =scan.nextInt();
+        System.out.println("enter b:");
+        b=scan.nextInt();
+        c=a+b;
+        System.out.println("the sum is: "+c);
+
+    }
+}
