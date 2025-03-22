c7b7d08a1cfddb9c2c1f904f0c0a58d0384b769c
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-03-22 20:56:08 +0530

    basics

---
+++ src/sortingAndSearching/appearingOnce.java
@@ -0,0 +1,30 @@
+package sortingAndSearching;
+
+public class appearingOnce {
+    public static int findSingleElement(int[] arr) {
+        int low = 0, high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 0) {
+                if (arr[mid] == arr[mid + 1]) {
+                    low = mid + 2;
+                } else {
+                    high = mid;
+                }
+            } else {
+                if (arr[mid] == arr[mid - 1]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return arr[low];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5, 5};
+        System.out.println("Single Element: " + findSingleElement(arr));
+    }
+}
