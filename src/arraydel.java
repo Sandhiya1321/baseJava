579170ce975649d2590cf0bcdebedc83706daac5
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-04-19 15:08:33 +0530

    base

--- src/arraydel.java
+++ src/arraydel.java
@@ -9,33 +9,24 @@ public class arraydel {
         int n=scan.nextInt();
         int array[] =new int[n];
         int res[]=new int[n];
-
         for(int i=0;i<n;i++) {
             array[i]=scan.nextInt();
         }
-
         for(int i=0;i<n;i++) {
             System.out.println(array[i]);
         }
-
         //delete
-
         for(int i=0;i<n-1;i++) {
             if(i>=2) {
                 res[i]=array[i+1];
-
             }
             else {
                 res[i]=array[i];
             }
-
         }
         System.out.println("after deletion");
         for(int i=0;i<n;i++) {
             System.out.println( res[i]);
         }
-
     }
-
-
 }
