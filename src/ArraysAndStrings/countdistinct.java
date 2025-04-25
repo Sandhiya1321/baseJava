d9270ffb0a46cb1d36d343a86393aedd45d3ed62
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-04-25 16:05:56 +0530

    base

---
+++ src/ArraysAndStrings/countdistinct.java
@@ -0,0 +1,25 @@
+package ArraysAndStrings;
+
+public class countdistinct {
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 2, 3, 4, 4};
+            int count = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                boolean distinct = true;
+                for (int j = 0; j < i; j++) {
+                    if (arr[i] == arr[j]) {
+                        distinct = false;
+                        break;
+                    }
+                }
+
+                if (distinct) {
+                    count++;
+                }
+            }
+
+            System.out.println("Number of distinct elements: " + count);
+        }
+    }
+
