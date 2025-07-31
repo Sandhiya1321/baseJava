7d1cb5396c55cc9ac0e0314fa4ebffc2b0a2d9b8
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-07-31 21:23:09 +0530

    base

--- src/Strings/reverseonlyletters.java
+++ src/Strings/reverseonlyletters.java
@@ -3,15 +3,28 @@ package Strings;
 import java.util.Scanner;
 public class reverseonlyletters {
     public static String reverse(String str){
-        String[] words = str.split(" ");
-        StringBuilder result = new StringBuilder();
+        char[] chars = str.toCharArray();
+        int start = 0;
 
-        for (String word : words) {
-            result.append(new StringBuilder(word).reverse()).append(" ");
+        for (int end = 0; end <= chars.length; end++) {
+            // Check for end of word or end of string
+            if (end == chars.length || chars[end] == ' ') {
+                reverseWord(chars, start, end - 1);
+                start = end + 1; // Move start to the next word
+            }
         }
-        return result.toString().trim();
-
+        return new String(chars);
     }
+    static void reverseWord(char[] chars, int start, int end) {
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.println("Enter the String");
