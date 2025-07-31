92abd8543b9e9c05751196425931fd9c451ffb1e
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-07-31 21:21:09 +0530

    base

--- src/Strings/reverseonlyletters.java
+++ src/Strings/reverseonlyletters.java
@@ -3,29 +3,19 @@ package Strings;
 import java.util.Scanner;
 public class reverseonlyletters {
     public static String reverse(String str){
-        char ch[]=str.toCharArray();
-        int low=0;
-        int high=ch.length-1;
-        while(low<=high){
-            if(Character.isAlphabetic(ch[low])&&Character.isAlphabetic(ch[high])){
-                char temp=ch[low];
-                ch[low]=ch[high];
-                ch[high]=temp;
-                low++;
-                high--;
-            }
-            else if(!Character.isAlphabetic(ch[low]))
-                low++;
-            else if(!Character.isAlphabetic(ch[high]))
-                high--;
+        String[] words = str.split(" ");
+        StringBuilder result = new StringBuilder();
 
+        for (String word : words) {
+            result.append(new StringBuilder(word).reverse()).append(" ");
         }
-        return String.valueOf(ch);
+        return result.toString().trim();
+
     }
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.println("Enter the String");
-        String str=scan.next();
+        String str=scan.nextLine();
         System.out.print(reverse(str));
     }
 }
