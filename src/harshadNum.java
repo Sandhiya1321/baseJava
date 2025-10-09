974946948caf86720ed7ce6eae6dd3aa9685d8a3
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-10-09 09:20:28 +0530

    base

---
+++ src/harshadNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class harshadNum {
+    public static void main(String[] args) {
+
+        //split the num into digits and sum them and divide it if it is equal to 0 then its harshad number
+        Scanner scan=new Scanner(System.in);
+        int num=scan.nextInt();
+        int temp=num;
+        int sum=0;
+        while(num!=0){
+            int digit=num%10;
+            sum+=digit;
+            num/=10;
+        }
+        if(temp%sum==0){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
