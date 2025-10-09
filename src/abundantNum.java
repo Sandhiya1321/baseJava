145a46be371ffb1a62de999f804803d277e92248
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-10-09 09:22:28 +0530

    base

---
+++ src/abundantNum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class abundantNum {
+    public static void main(String[] args) {
+        //if the sum of factors is greater than the num then it is abundant number
+        Scanner scan=new Scanner(System.in);
+        int num=scan.nextInt();
+        int sum=1;
+        for(int i=2;i<num;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum>num){
+            System.out.println("It's an abundant number");
+            System.out.println ("The Abundance is: " + (sum - num));
+        }else{
+            System.out.println("It's not an abundant number");
+        }
+    }
+}
---
+++ src/friendlyPair.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class friendlyPair {
+    public static void main(String[] args) {
+        //if sum of the factors is divisible by the num and both are equal then it is friendly pair
+        Scanner scan=new Scanner(System.in);
+        int num1=scan.nextInt();
+        int num2=scan.nextInt();
+
+        int ans1=help(num1);
+        int ans2=help(num2);
+
+        if(ans1/num1==ans2/num2){
+            System.out.println("friend");
+        }else{
+            System.out.println("not");
+        }
+
+    }
+    public static int help(int num){
+        int sum=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
