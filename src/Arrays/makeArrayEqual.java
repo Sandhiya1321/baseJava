47f2c3ac8bc4f05419bb816eda5901c9154448ed
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-10-22 12:42:53 +0530

    base

---
+++ src/Arrays/makeArrayEqual.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class makeArrayEqual {
+    public static boolean equal(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            while(arr[i]%2==0) arr[i]/=2;
+            while(arr[i]%3==0) arr[i]/=3;
+
+        }
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=arr[0]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 50, 100, 75 ,45};
+        System.out.println(equal(arr));
+    }
+}
