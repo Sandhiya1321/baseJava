cccaf48cdc097d793d7901676f48b0026b24ac9c
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-10-27 16:41:58 +0530

    base

---
+++ src/array/trappingRainWater.java
@@ -0,0 +1,28 @@
+package array;
+
+public class trappingRainWater {
+    public static void main(String[] args) {
+        int[] arr={0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(arr));
+    }
+    public static  int trap(int[] height) {
+        int n=height.length;
+        int[] water=new int[n];
+        int[] leftmax=new int[n];
+        int[] rightmax=new int[n];
+        int tot=0;
+        leftmax[0]=height[0];
+        rightmax[n-1]=height[n-1];
+        for(int i=1;i<n;i++){
+            leftmax[i]=Math.max(leftmax[i-1],height[i]);
+        }
+        for(int j=n-2;j>=0;j--){
+            rightmax[j]=Math.max(rightmax[j+1],height[j]);
+        }
+        for(int i=0;i<n;i++){
+            water[i]=Math.min(leftmax[i],rightmax[i])-height[i];
+            tot+=water[i];
+        }
+        return tot;
+    }
+}
